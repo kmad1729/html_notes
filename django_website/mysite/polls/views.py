5deefa98af01c2b4127e91eb792a14a7b6526c13
Kashyap Maduri <kashyapmaduri@outlook.com>
2015-12-03 21:24:38 -0800

    adding css notes

--- django_website/mysite/polls/views.py
+++ django_website/mysite/polls/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question, Choice
+from django.template import RequestContext, loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world, You're at polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+        })
+    return HttpResponse(template.render(context))
 
 def detail(request, question_id):
     return HttpResponse("you're looking at question %s" % question_id)
@@ -15,3 +22,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You are voting on question %s" % question_id)
+
